776d5ee74fe05daa7e85c3510c97afbd5f60e007
jsBear92 <js104427@gmail.com>
2023-02-14 15:20:00 +0800

    building a fetch component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 import React, { createContext, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { useFetch } from './useFetch';
 
-export const TreesContext = createContext();
-
-export const useTrees = () => useContext(TreesContext);
-
-const trees = [
-  {id: "1", type: "Maple"},
-  {id: "2", type: "Oak"},
-  {id: "3", type: "Family"},
-  {id: "4", type: "Component"},
-];
+function App({ login }) {
+  const { loading, data, error } = useFetch(`https://api.github.com/users/${login}`);
+  if (loading) return <h1>loading...</h1>
+  if (error)
+    return (
+      <pre>{JSON.stringify(error, null, 2)}</pre>
+    );
+  return (
+    <div>
+      <img src={data.avatar_url} alt={data.login} />
+      <div>
+        <h1>{data.login}</h1>
+        {data.name && <p>{data.name}</p>}
+        {data.location && <p>{data.location}</p>}
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <TreesContext.Provider value={{ trees }}>
-    <App />
-  </TreesContext.Provider>
+  <React.StrictMode>
+    <App login="jsbear92" />
+  </React.StrictMode>
 );
