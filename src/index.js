9eb96db039b4b09514243b5cca26cdfc13c5b6ca
jsBear92 <js104427@gmail.com>
2023-02-14 14:08:24 +0800

    refactoring useState to useReducer

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 function App() {
-  const [number, setNumber] = useReducer((number, newNumber) => number + newNumber, 0);
-  return <h1 onClick={() => setNumber(1)}>{number}</h1>
+  const [checked, toggle] = useReducer((checked) => !checked, false);
+  return (
+    <>
+      <input
+        type='checkbox'
+        value={checked}
+        onChange={toggle}
+        />
+        {checked ? "checked" : "not checked"}
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
