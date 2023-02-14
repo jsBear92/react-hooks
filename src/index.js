d9808ae19f4bc698ccbb2aa82bc64d5d23d27b35
jsBear92 <js104427@gmail.com>
2023-02-14 13:29:20 +0800

    start useEffect

--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { FaStar } from "react-icons/fa"
-
-const createArray = (length) => [
-  ...Array(length)
-]
-
-function Star({ selected = false, onSelect }) {
-  return (<FaStar color={selected ? "red" : "gray"} onClick={onSelect} />);
-}
-
-function StarRating({ totalStars = 5 }) {
-  const [selectedStars , setSelectedStars] = useState(0);
-  return (
-    <>
-      {createArray(totalStars).map((n, i) => (
-        <Star
-          key={i}
-          selected={selectedStars > i}
-          onSelect={() => setSelectedStars(i + 1)}
-        />
-      ))};
-      <p>{selectedStars} of {totalStars}</p>
-    </>
-  );
-}
 
 function App() {
-  return <StarRating totalStars={10} />;
+  const [name, setName] = useState("James");
+
+  useEffect(() => {
+    document.title = `Celebrate ${name}`;
+  });
+  return (
+    <section>
+      <p>Congratulations {name}!</p>
+      <button onClick={() => setName("Will")}>Change Winner</button>
+    </section>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
