f0be032d901aaad85864ea14a98a2937ec2fb948
jsBear92 <js104427@gmail.com>
2023-02-14 13:06:03 +0800

    incorporating the useState hook

--- src/index.js
+++ src/index.js
@@ -1,23 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const [first, second, third] = ["Alex", "Ali", "Anna"];
-
-console.log(first);
-console.log(second);
-console.log(third);
+function App() {
+  const [status, setStatus] = useState("Not Delivered");
+  return (
+    <div>
+      <h1>The package is: {status}</h1>
+      <button onClick={() => setStatus("Delivered")}>Deliver</button>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App name="James" />
+    <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
