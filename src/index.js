99e6837812ecc2becdc2de79398b00c970d0735c
jsBear92 <js104427@gmail.com>
2023-02-14 13:26:14 +0800

    Sending interactions up component trees

--- src/index.js
+++ src/index.js
@@ -7,16 +7,28 @@ const createArray = (length) => [
   ...Array(length)
 ]
 
-function Star({ selected = false }) {
-  return (<FaStar color={selected ? "red" : "gray"} />);
+function Star({ selected = false, onSelect }) {
+  return (<FaStar color={selected ? "red" : "gray"} onClick={onSelect} />);
 }
 
-function StarRating({ totalStarts = 5 }) {
-  return createArray(totalStarts).map((n, i) => (<Star key={i} />));
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars , setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))};
+      <p>{selectedStars} of {totalStars}</p>
+    </>
+  );
 }
 
 function App() {
-  return <StarRating />;
+  return <StarRating totalStars={10} />;
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
